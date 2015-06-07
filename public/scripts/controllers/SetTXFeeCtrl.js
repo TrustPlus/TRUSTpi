7f051e83d5088913dac17d71c4581f5b055a835f
MitchellMint <github@mitchellmint.com>
2015-06-07 10:59:22 -0500

    Update SetTXFeeCtrl.js

--- public/scripts/controllers/SetTXFeeCtrl.js
+++ public/scripts/controllers/SetTXFeeCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('PaycoinRpiWallet')
+angular.module('TrustRpiWallet')
     .controller('SetTXFeeCtrl', function ($scope, $rootScope) {
         $rootScope.app.curTitle = "Set TX Fee";
     }
