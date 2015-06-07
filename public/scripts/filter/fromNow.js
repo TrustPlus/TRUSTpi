21a2655e2a854d7e50a1f3d1ca0db4a8e4851b81
MitchellMint <github@mitchellmint.com>
2015-06-07 11:03:23 -0500

    Update fromNow.js

--- public/scripts/filter/fromNow.js
+++ public/scripts/filter/fromNow.js
@@ -2,7 +2,7 @@
 
 /* Filters */
 // need load the moment.js to use this filter. 
-angular.module('PaycoinRpiWallet')
+angular.module('TrustRpiWallet')
   .filter('fromNow', function() {
     return function(date) {
       return moment(date).fromNow();
