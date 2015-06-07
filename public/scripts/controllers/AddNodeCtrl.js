d6e2972c8a227c84d1d4f28bbd9c7d829be206d5
MitchellMint <github@mitchellmint.com>
2015-06-07 10:45:45 -0500

    Update AddNodeCtrl.js

--- public/scripts/controllers/AddNodeCtrl.js
+++ public/scripts/controllers/AddNodeCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('PaycoinRpiWallet')
+angular.module('TrustRpiWallet')
     .controller('AddNodeCtrl', function ($scope, $rootScope, $http) {
         $rootScope.app.curTitle = "Add Node";
 
