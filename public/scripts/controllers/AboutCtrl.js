9e676a98c0904d7f41976def3bffbc2f80152042
MitchellMint <github@mitchellmint.com>
2015-06-07 10:45:20 -0500

    Update AboutCtrl.js

--- public/scripts/controllers/AboutCtrl.js
+++ public/scripts/controllers/AboutCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('PaycoinRpiWallet')
+angular.module('TrustRpiWallet')
     .controller('AboutCtrl', function ($scope, $rootScope) {
         $rootScope.app.curTitle = "About";
     }
