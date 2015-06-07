0f03b2505479db3bc306500926e4bdd0711d0c33
MitchellMint <github@mitchellmint.com>
2015-06-07 10:47:04 -0500

    Update AddressbookCtrl.js

--- public/scripts/controllers/AddressbookCtrl.js
+++ public/scripts/controllers/AddressbookCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('PaycoinRpiWallet')
+angular.module('TrustRpiWallet')
     .controller('AddressbookCtrl', function ($scope, $rootScope, $http) {
         $rootScope.app.curTitle = "Address Book";
 
